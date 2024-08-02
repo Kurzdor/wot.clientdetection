676762f46cb885d8daf91c978cc4147b3500167c
Kurzdor <kurzdor@pm.me>
2024-08-02 04:44:39 +0300

    release: 2024.08.02

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+
+### 2024.07.28.1
+
+- Updated license
+
+### 2024.07.28
+
+- Fixed return types for `LauncherManager` methods
+
+### 2024.07.27.1
+
+- Updated readme
+
+### 2024.07.27
+
+- Initial release
--- wotclientdetection/__init__.py
+++ wotclientdetection/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'wotclientdetection'
-__version__ = '2024.07.28.1'
+__version__ = '2024.08.02'
 
 from wotclientdetection.manager import LauncherManager
 from wotclientdetection.constants import LauncherFlavour, ClientBranch, ClientRealm
