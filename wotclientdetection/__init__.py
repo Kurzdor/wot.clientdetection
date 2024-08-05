4f9b4a02df21ae8ff28dfcc6a9156ed35e4100f3
Kurzdor <kurzdor@pm.me>
2024-08-05 06:24:36 +0300

    release: 2024.8.5.1

--- wotclientdetection/__init__.py
+++ wotclientdetection/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'wotclientdetection'
-__version__ = '2024.8.5'
+__version__ = '2024.8.5.1'
 
 from wotclientdetection.manager import LauncherManager
 from wotclientdetection.constants import LauncherFlavour, ClientBranch, ClientRealm
